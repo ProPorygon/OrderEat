ce45fb7d6bcb326fa3bfaa374b4cb37c7d796019
Kelly Cole <kmcole2@illinois.edu>
2015-10-04 20:48:35 +0000

    update

--- OrderEat.py
+++ OrderEat.py
@@ -1,12 +1,11 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello from Flask!'
 
-
-if __name__ == '__main__':
-    app.run()
