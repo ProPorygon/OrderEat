b2ab250020b7b09d7ba81550fe4b1c9ad0cf9987
Kevin Renner <renke@wowway.com>
2015-10-04 16:01:46 -0500

    Testing stuff

--- OrderEat.py
+++ OrderEat.py
@@ -1,6 +1,3 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,4 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello from Flask!'
+
+if __name__ == '__main__':
+    app.run()
 
