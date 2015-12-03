47e4255d9a01b136d4ac97a16b6233d70fea55e9
Kevin Renner <krenner529@gmail.com>
2015-12-03 02:23:08 -0600

    Added files for deployment

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='OrderEat',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
+      description='Streamline the ordering process in restaurants',
+      author='Kevin Renner',
+      author_email='krenner529@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
 #      install_requires=['Django>=1.3'],
      )
